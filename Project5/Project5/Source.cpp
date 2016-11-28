5323227229e534cdc7f700d053c36415becdc3d9
bardur <bardur@DESKTOP-5LB2JFK>
2016-11-28 13:34:05 +0000

    proj5

--- Project5/Project5/Source.cpp
+++ Project5/Project5/Source.cpp
@@ -3,31 +3,37 @@
 
 using namespace std;
 
+// Variables for lastname + firstname and firstname + lastname.
 string firstname1, firstname2, firstname3;
 string lastname1, lastname2, lastname3;
-string name1, name2, name3;
 
-int i;
+string name1, name2, name3;
+string fname1, fname2, fname3;
+
 
 int main() {
 
+	// Input
 	cout << "Firstname (1): ";
 	cin >> firstname1;
 	cout << "Lastname (1): ";
 	cin >> lastname1;
 	name1 = lastname1 + ", " + firstname1;
+	fname1 = lastname1 + ", " + firstname1;
 
 	cout << "Firstname (2): ";
 	cin >> firstname2;
 	cout << "Lastname (2): ";
 	cin >> lastname2;
 	name2 = lastname2 + ", " + firstname2;
+	fname2 = lastname2 + ", " + firstname2;
 
 	cout << "Firstname (3): ";
 	cin >> firstname3;
 	cout << "Lastname (3): ";
 	cin >> lastname3;
 	name3 = lastname3 + ", " + firstname3;
+	fname3 = lastname3 + ", " + firstname3;
 	cout << endl;
 
 	// Checking if any last names are equal. 
@@ -65,34 +71,34 @@ int main() {
 	// If equal found, switch to sorting by first name. 
 	} else {
 		if ((firstname1 < firstname2) && (firstname1 < firstname3) && (firstname2 < firstname3)) {
-			cout << firstname1 + " " + lastname1 << endl;
-			cout << firstname2 + " " + lastname2 << endl;
-			cout << firstname3 + " " + lastname3 << endl;
+			cout << fname1 << endl;
+			cout << fname2 << endl;
+			cout << fname3 << endl;
 		}
 		else if ((firstname1 < firstname2) && (firstname1 < firstname3) && (firstname2 < firstname3)) {
-			cout << firstname1 + " " + lastname1 << endl;
-			cout << firstname3 + " " + lastname3 << endl;
-			cout << firstname2 + " " + lastname2 << endl;
+			cout << fname1 << endl;
+			cout << fname3 << endl;
+			cout << fname2 << endl;
 		}
 		else if ((firstname2 < firstname1) && (firstname2 < firstname3) && (firstname1 < firstname2)) {
-			cout << firstname2 + " " + lastname2 << endl;
-			cout << firstname1 + " " + lastname1 << endl;
-			cout << firstname3 + " " + lastname3 << endl;
+			cout << fname2 << endl;
+			cout << fname1 << endl;
+			cout << fname3 << endl;
 		}
 		else if ((firstname2 < firstname1) && (firstname2 < firstname3) && (firstname3 < firstname1)) {
-			cout << firstname2 + " " + lastname2 << endl;
-			cout << firstname3 + " " + lastname3 << endl;
-			cout << firstname1 + " " + lastname1 << endl;
+			cout << fname2 << endl;
+			cout << fname3 << endl;
+			cout << fname1 << endl;
 		}
 		else if ((firstname3 < firstname1) && (firstname3 < firstname2) && (firstname2 < firstname1)) {
-			cout << firstname3 + " " + lastname3 << endl;
-			cout << firstname2 + " " + lastname2 << endl;
-			cout << firstname1 + " " + lastname1 << endl;
+			cout << fname3 << endl;
+			cout << fname2 << endl;
+			cout << fname1 << endl;
 		}
 		else if ((firstname3 < firstname1) && (firstname3 < firstname2) && (firstname1 < firstname2)) {
-			cout << firstname3 + " " + lastname3 << endl;
-			cout << firstname1 + " " + lastname1 << endl;
-			cout << firstname2 + " " + lastname2 << endl;
+			cout << fname3 << endl;
+			cout << fname1 << endl;
+			cout << fname2 << endl;
 		}
 	}
 
