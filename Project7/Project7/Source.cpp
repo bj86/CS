9512fe7e12b63366ed653b1a4a2a0691a6319bf0
bardur <bardur@DESKTOP-5LB2JFK>
2016-12-04 14:09:09 +0000

    Recoded the process of splitting rates.

--- Project7/Project7/Source.cpp
+++ Project7/Project7/Source.cpp
@@ -9,21 +9,22 @@ using namespace std;
 // Menu variables
 char hang;
 
-// Function prototypes
-int dice();
-int minutes();
-int price();
+// Function declarations
+void dice();
+void input();
+void minutes(int &h1, int &h2, int &m1, int &m2);
+void price(int &length, int &r1, int &r2, int &r3);
 
 
 int main()
-{
-	price();
+{	
+	input();
 
 	// Pause
 	cin >> hang;
 }
 
-int dice() {
+void dice() {
 	// dice variables
 	int one=0, two=0, three=0, four=0, five=0, six=0;
 	float roll, x;
@@ -51,52 +52,105 @@ int dice() {
 	cout << "(4): " << four << setw(3) << float((four / x) * 100) << "%" << setw(6);
 	cout << "(5): " << five << setw(3) << float((five / x) * 100) << "%" << setw(6);
 	cout << "(6): " << six << setw(3) << float((six / x) * 100) << "%" << endl;
-	return 0;
 }
 
-int minutes() {
+void input() {
 	string n1, n2, timestring1, timestring2;
 	int h1, h2, m1, m2;
-	int calltime, fullrate, discount;
+	cout << fixed;
+	cout << setprecision(2);
 
-	// Input
-	cout << "Start of call (format, HH:MM)" << endl;
+	// Input times
+	cout << "Start (HH:MM) ";
 	cin >> n1;
-
-	cout << "End of call (format, HH:MM)" << endl;
+	cout << "Stop (HH:MM) ";
 	cin >> n2;
+	cin.get();
 
-	// Call start
-	timestring1.insert(0, n1, 0, 2), timestring2.insert(0, n1, 3, 5);
-	h1 = stoi(timestring1, 0, 10), m1 = stoi(timestring2, 0, 10);
-	timestring1 = "", timestring2 = "";
+	if (n1.length() == 5 && n2.length() == 5)
+	{
+		// Start of call. Stoi() cuts out numbers and stores as int.
+		timestring1.insert(0, n1, 0, 2), timestring2.insert(0, n1, 3, 5);
+		h1 = stoi(timestring1, 0, 10), m1 = stoi(timestring2, 0, 10);
 
-	// Call end
-	timestring1.insert(0, n2, 0, 2), timestring2.insert(0, n2, 3, 5);
-	h2 = stoi(timestring1, 0, 10), m2 = stoi(timestring2, 0, 10);
+		// Clearing temporary strings for next input.
+		timestring1 = "", timestring2 = "";
 
-	// Calculate time spent in each rate.
-	if ((h1 == 8 && m1 >= 00) || (h1 > 8) && ((h2 < 23) && m2 < 59)) {
-		fullrate = ((h1 + (h2 - h1) - 18) * 60) + (m2 - 30);
-		discount = ((h2 - h1) * 60 + (m2 - m1) - fullrate);
+		// End of call. Same as above.
+		timestring1.insert(0, n2, 0, 2), timestring2.insert(0, n2, 3, 5);
+		h2 = stoi(timestring1, 0, 10), m2 = stoi(timestring2, 0, 10);
+
+		if (h1 > h2) {
+			cout << "That wont work here, buddy!" << endl;
+			input();
+		}
+		else {
+
+			// This bit handles most of the printing.
+			cout << endl;
+			cout << "START" << setw(12) << "STOP" << setw(12) << "LENGTH" << setw(12) << "SEK" << endl;
+			cout << n1 << setw(12) << n2 << setw(12);
+
+			minutes(h1, h2, m1, m2);
+		}
+
 	}
-
-	cout << ((h1 + (h2 - h1) - 24) * 60) + (m2 - m1) << endl;
-
-	cout << fullrate << endl;
-	cout << discount << endl;
-
-	// Returning calltime to use it in other function.
-	calltime = (h2 - h1) * 60 + (m2 - m1);
-	cout << calltime << endl;
-	return calltime;
+	else 
+	{
+		cout << "Please use the correct format! (HH:MM) e.g: 07:45 - 19:02" << endl;
+		input();
+	}
 
 }
 
-int price() {
-	int total;
-	int const SEK = 4;
-	total = minutes() * SEK;
-	return total;
+void minutes(int &h1, int &h2, int &m1, int &m2) 
+{
+	int length, r1 = 0, r2 = 0, r3 = 0;
+	int x, j = 0, t1, t2, t3;
 
+	// Storing length of call to use in other functions.
+	length = (h2 - h1) * 60 + (m2 - m1);
+
+	// Calculate time spent in each rate. Variable x is the starting point.
+	// t1 = 00:00 to 00:08, t2 = 00:00 18:50, t3 = 00:00 to 23:59.
+	x = (h1 * 60 + m1);
+	t1 = 480, t2 = 1110, t3 = 1439;
+	
+	for (x, j; j < length; x++, j++)
+	{
+		if (x < t1)
+		{
+			r1++;
+		}
+		if (x >= t1 && x < t2)
+		{
+			r2++;
+		}
+		if (x >= t2 && x < t3)
+		{
+			r3++;
+		}
+	}
+
+	price( length, r1, r2, r3);
+}
+
+void price(int &length, int &r1, int &r2, int &r3)
+{
+	double const MOMS = 1.25, RABAT = 0.85, SEK = 4.00, z1 = 0.35, z2 = 1.00;
+
+	// Storing results for better formatting and readability.
+	double result1 = (SEK * r1 * z1 * MOMS * RABAT) + (SEK * r2 * z2 * MOMS * RABAT) + (SEK * r3 * z1 * MOMS * RABAT);
+	double result2 = (SEK * r1 * z1 * MOMS) + (SEK * r2 * z2 * MOMS) + (SEK * r3 * z1 * MOMS);
+
+	if (length > 30)
+	{
+		cout << length << setw(12) << result1;
+	}
+	else {
+		cout << length << setw(12) << result2;
+	}
+	//  Return input() function to try again.
+	cout << endl << endl;
+	input();
 }
